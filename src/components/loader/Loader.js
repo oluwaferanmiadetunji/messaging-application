9efec15af5da8154cd7a9ceede5fd3ea0ccd2a8d
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-09-24 13:47:43 +0100

    Add loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../../assets/styles/loader.css';
+import Spinner from 'react-bootstrap/Spinner';
+
+const Loader = () => {
+	return (
+		<div className='main'>
+			<div className='cover black'></div>
+			<div className='container'>
+				<h1 className='logo'>Mensaje</h1>
+				<div className='content'>
+					<h4 className='motto'>A better way to connect and communicate with friends.</h4>
+					<div className='subscribe'>
+						<div className='info-text'>
+							<Spinner animation='grow' variant='light' size='sm' />
+							<Spinner animation='grow' variant='light' size='sm' />
+							<Spinner animation='grow' variant='light' size='sm' />
+							<Spinner animation='grow' variant='light' size='sm' />
+							<Spinner animation='grow' variant='light' size='sm' />
+							<Spinner animation='grow' variant='light' size='sm' />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Loader;
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -1,13 +1,18 @@
 import React, {Suspense, lazy} from 'react';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import Loader from '../loader';
 
 const Home = lazy(() => import('../home' /* webpackChunkName: "Home Page" */));
+const Login = lazy(() => import('../login' /* webpackChunkName: "Login Page" */));
+const Register = lazy(() => import('../register' /* webpackChunkName: "register Page" */));
 
 const Routes = () => (
 	<Router>
-		<Suspense fallback={<p></p>}>
+		<Suspense fallback={<Loader/>}>
 			<Switch>
 				<Route exact path='/' component={Home} />
+				<Route exact path='/signin' component={Login} />
+				<Route exact path='/signup' component={Register} />
 			</Switch>
 		</Suspense>
 	</Router>
