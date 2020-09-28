e092bcf528c048a13d016badaf8bb506ff4f19b4
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-09-28 22:19:36 +0100

    Add tabs

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-
+import Nav from 'react-bootstrap/Nav';
 import logo from 'assets/images/reactlogo.png';
 import sidebarImage from 'assets/images/default.jpg';
+import NavbarLinks from '../Navbars/NavbarLinks';
 
 const Sidebar = () => {
+	const [width, setWidth] = useState(window.innerWidth);
+	const update = () => {
+		setWidth(window.innerWidth);
+	};
+
+	useEffect(() => {
+		update();
+		window.addEventListener('resize', update());
+	}, []);
+
 	return (
 		<div id='sidebar' className='sidebar' data-color={'black'} data-image={sidebarImage}>
 			<div className='sidebar-background' style={{backgroundImage: `url(${sidebarImage})`}} />
@@ -20,16 +31,15 @@ const Sidebar = () => {
 			</div>
 			<div className='sidebar-wrapper'>
 				<ul className='nav'>
-					<li className='active '>
-						<Link to={'/'} className='nav-link'>
-							<p>afe</p>
-						</Link>
-					</li>
-					<li className='active'>
-						<Link to={'/'} className='nav-link'>
-							<p>afe</p>
-						</Link>
-					</li>
+					{width <= 991 ? <NavbarLinks /> : null}
+					<Nav justify  variant='tabs' defaultActiveKey='/home'>
+						<Nav.Item>
+							<Nav.Link href='/home'>All</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link eventKey='link-1'>Recent</Nav.Link>
+						</Nav.Item>
+					</Nav>
 				</ul>
 			</div>
 		</div>
