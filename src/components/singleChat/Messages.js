64fad47902a0106fcf1b623ba6e8bd09fdad2cb6
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-01 23:43:18 +0100

    Style messages

--- src/components/singleChat/Messages.js
+++ src/components/singleChat/Messages.js
@@ -8,33 +8,32 @@ const Messages = ({chats}) => {
 	const {recipient} = useAppContext();
 	const username = localStorage.username;
 
-	const messageClass = (user) => {
-		let textClass = 'messageBox';
-		if (user === 'admin') {
-			textClass = 'messageBox admin';
-		} else if (user === recipient.username) {
-			textClass = 'messageBox recipient';
-		} else if (user === username) {
-			textClass = 'messageBox sender';
-		}
-
-		return textClass;
-	};
 	return (
 		<ScrollToBottom className='messages'>
 			{chats.map(({createdAt, message, sender}, index) => (
 				<div key={index}>
-					<div className='messageContainer'>
-						<div className={messageClass(sender)}>
-							<p className='messageText'>{ReactEmoji.emojify(message)}</p>
-							{sender === 'admin' ? null : (
-								<>
-									<p className='date'>{dayjs(createdAt).format('MMM DD, YYYY HH:mm')}</p>
-									<p className='sentText'>{sender}</p>
-								</>
-							)}
+					{sender === 'admin' && (
+						<div className='messageContainer'>
+							<div className='messageBox admin'>
+								<p className='messageText'>{ReactEmoji.emojify(message)}</p>
+							</div>
 						</div>
-					</div>
+					)}
+					{sender === recipient.username && (
+						<div className='container'>
+							<img src={recipient.imageUrl} alt='Avatar' />
+							<p>{ReactEmoji.emojify(message)}</p>
+							<span className='time-right'>{dayjs(createdAt).format('MMM DD, YYYY HH:mm')}</span>
+						</div>
+					)}
+
+					{sender === username && (
+						<div className='container sender'>
+							<img src={recipient.imageUrl} alt='Avatar' className='right' />
+							<p>{ReactEmoji.emojify(message)}</p>
+							<span className='time-left'>{dayjs(createdAt).format('MMM DD, YYYY HH:mm')}</span>
+						</div>
+					)}
 				</div>
 			))}
 		</ScrollToBottom>
